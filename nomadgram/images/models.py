7771820d56e12c816f10ec1cb98df5ae99e9913a
GeonLee <leegun2003@gmail.com>
2018-03-23 15:37:48 +0900

    #1-20 Creating the Image Model

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class TimeStampeModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimeStampeModel):
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+
+class Comment(TimeStampeModel):
+
+    message = models.TextField()
