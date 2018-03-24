1790e623312af4bfee2663f06eca46fbbd2d6bbd
Geon Lee <geon@DESKTOP-UDFV0NK.localdomain>
2018-03-24 23:51:39 +0900

    #1-24 Registering the Models in the admin

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
 # Create your models here.
 class TimeStampeModel(models.Model):
@@ -12,10 +13,26 @@ class TimeStampeModel(models.Model):
 
 class Image(TimeStampeModel):
 
+    """ Image Model """
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+
 
 class Comment(TimeStampeModel):
 
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+    image = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+
+class Like(TimeStampeModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.PROTECT)
+    image = models.ForeignKey(Image, null=True, on_delete=models.PROTECT)
