29b8fd596ae555eaa474f41d9358b7c52672f0cf
Geon Lee <geon@DESKTOP-UDFV0NK.localdomain>
2018-03-26 01:40:36 +0900

    #1-41 Using Variables on the URLS

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework import serializers
+from nomadgram.users import models as user_models
 from . import models
+
+class FeedUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = user_models.User
+        fields = (
+            'username',
+            'profile_image'
+        )
 
 class CommentSerializer(serializers.ModelSerializer):
 
+    creator = FeedUserSerializer()
+
     class Meta:
         model = models.Comment
-        fields = '__all__'
+        fields = (
+            'id',
+            'message',
+            'creator',
+        )
 
 class LikeSerializer(serializers.ModelSerializer):
 
@@ -17,7 +33,7 @@ class LikeSerializer(serializers.ModelSerializer):
 class ImageSerializer(serializers.ModelSerializer):
 
     comments = CommentSerializer(many=True)
-    likes = LikeSerializer(many=True)
+    creator = FeedUserSerializer()
 
     class Meta:
         model = models.Image
@@ -27,6 +43,7 @@ class ImageSerializer(serializers.ModelSerializer):
             'location',
             'caption',
             'comments',
-            'likes',
+            'like_count',
+            'creator',
         )
 
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -2,32 +2,66 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
 
-class ListAllImages(APIView):
+class Feed(APIView):
 
     def get(self, request, format=None):
+        
+        user = request.user
+        
+        folllowing_users = user.following.all()
 
-        all_images = models.Image.objects.all()
+        image_list = []
+        
+        print(request.user.following.all())
+        
+        for folllowing_user in folllowing_users:
+            user_images = folllowing_user.images.all()[:2]
+            for image in user_images:
+                image_list.append(image)
+        sorted_list = sorted(image_list,key=lambda image: image.created_at, reverse=True)
+        print(sorted_list)
+        serializer = serializers.ImageSerializer(sorted_list, many=True)
 
-        serializer = serializers.ImageSerializer(all_images, many=True)
+        return Response(serializer.data)
 
-        return Response(data=serializer.data)
+class LikeImage(APIView):
 
-class ListAllComments(APIView):
+    def get(self, request, image_id, format=None):
+        print(image_id)
+        return Response(status=200)
 
-    def get(self, request, format=None):
+#/images/3/like
 
-        all_comments = models.Comment.objects.all()
+# 1. take the id from the url
+# 2. we want to find an image with this id
+# 3. we want to create a like for that image
 
-        serializer = serializers.CommentSerializer(all_comments, many=True)
+# class ListAllImages(APIView):
 
-        return Response(data=serializer.data)
+#     def get(self, request, format=None):
 
-class ListAllLikes(APIView):
+#         all_images = models.Image.objects.all()
 
-    def get(self, request, format=None):
+#         serializer = serializers.ImageSerializer(all_images, many=True)
 
-        all_likes = models.Like.objects.all()
+#         return Response(data=serializer.data)
 
-        serializer = serializers.LikeSerializer(all_likes, many=True)
+# class ListAllComments(APIView):
 
-        return Response(data=serializer.data)
+#     def get(self, request, format=None):
+
+#         all_comments = models.Comment.objects.all()
+
+#         serializer = serializers.CommentSerializer(all_comments, many=True)
+
+#         return Response(data=serializer.data)
+
+# class ListAllLikes(APIView):
+
+#     def get(self, request, format=None):
+
+#         all_likes = models.Like.objects.all()
+
+#         serializer = serializers.LikeSerializer(all_likes, many=True)
+
+#         return Response(data=serializer.data)
