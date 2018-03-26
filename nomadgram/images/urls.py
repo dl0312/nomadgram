5b13553e7f7f80d50c5534b71dacc7c766d0225e
Geon Lee <geon@DESKTOP-UDFV0NK.localdomain>
2018-03-26 18:10:41 +0900

    #1-42~50 Working with Regular Expression ~ Unfollowing User

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -10,9 +10,24 @@ urlpatterns = [
         name = 'feed',
     ),
     url(
-        regex = r'(?P<image_id>\w+)/like/',
+        regex = r'^(?P<image_id>[0-9]+)/like/',
         view = views.LikeImage.as_view(),
         name = 'like_image',
+    ),
+    url(
+        regex = r'^(?P<image_id>[0-9]+)/unlike/',
+        view = views.UnLikeImage.as_view(),
+        name = 'unlike_image',
+    ),
+    url(
+        regex = r'^(?P<image_id>[0-9]+)/comment/',
+        view = views.CommentOnImage.as_view(),
+        name = 'comment_image',
+    ),
+    url(
+        regex = r'comments/(?P<comment_id>[0-9]+)/$',
+        view = views.Comment.as_view(),
+        name = 'comment',
     ),
 ]
 
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 class Feed(APIView):
@@ -26,9 +27,80 @@ class Feed(APIView):
 
 class LikeImage(APIView):
 
-    def get(self, request, image_id, format=None):
-        print(image_id)
-        return Response(status=200)
+    def post(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator = user,
+                image = found_image
+            )
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
+        except models.Like.DoesNotExist:
+            new_like = models.Like.objects.create(
+                creator = user,
+                image = found_image
+            )
+            new_like.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+class UnLikeImage(APIView):
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator = user,
+                image = found_image
+            )
+            preexisting_like.delete()
+            
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+            return Response(status=status.HTTP_203_MODIFIED)
+
+
+class CommentOnImage(APIView):
+    
+    def post(self, request, image_id, format=None):
+        serializer = serializers.CommentSerializer(data=request.data)
+        user = request.user
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(stauts=status.HTTP_404_NOT_FOUND)
+
+        if serializer.is_valid():
+            serializer.save(creator=user, image=found_image)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Comment(APIView):
+
+    def delete(self, request, comment_id, format=None):
+
+        user = request.user
+        try:
+            comment = models.Comment.objects.get(id=comment_id, creator=user)
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except models.Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 #/images/3/like
 
