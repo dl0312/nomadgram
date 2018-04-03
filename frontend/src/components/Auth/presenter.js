d3ee25d4f140e453535c6c90dba23ed21910dc95
Geon Lee <geon@DESKTOP-UDFV0NK.localdomain>
2018-04-03 18:38:25 +0900

    #3-33 Moving the AuthForm to their own components

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,54 +1,64 @@
 import React from "react";
 import styles from "./styles.scss";
-import { LoginForm, SignupForm } from "components/AuthForms";
+import PropTypes from "prop-types";
+import LoginForm from "components/LoginForm";
+import SignupForm from "components/SignupForm";
 
 const Auth = (props, context) => (
   <main className={styles.auth}>
     <div className={styles.column}>
-      <img src={require("images/phone.png")} alt="Checkout our app. Is cool" />
+      <img
+        src={require("images/phone.png")}
+        alt={context.t("Checkout our app. Is cool")}
+      />
     </div>
     <div className={styles.column}>
       <div className={`${styles.whiteBox} ${styles.formBox}`}>
-        {props.action == "login" && <LoginForm />}
-        {props.action == "signup" && <SignupForm />}
+        <img src={require("images/logo.png")} alt={context.t("Logo")} />
+        {props.action === "login" && <LoginForm />}
+        {props.action === "signup" && <SignupForm />}
       </div>
       <div className={styles.whiteBox}>
-        {props.action === "login" && (
-          <p>
-            Don't have an account?{" "}
+        {props.action === "signup" && (
+          <p className={styles.text}>
+            {context.t("Have an account?")}{" "}
             <span onClick={props.changeAction} className={styles.changeLink}>
-              Sign up
+              {context.t("Log in")}
             </span>
           </p>
         )}
-        {props.action === "signup" && (
-          <p>
-            Have an account?{" "}
+        {props.action === "login" && (
+          <p className={styles.text}>
+            {context.t("Don't have an account?")}{" "}
             <span onClick={props.changeAction} className={styles.changeLink}>
-              Log in
+              {context.t("Sign up")}
             </span>
           </p>
         )}
       </div>
       <div className={styles.appBox}>
-        <span>Get the app</span>
+        <span>{context.t("Get the app")}</span>
         <div className={styles.appstores}>
           <img
             src={require("images/ios.png")}
-            alt="Download it on the Apple Appstore"
+            alt={context.t("Download it on the Apple Appstore")}
           />
           <img
             src={require("images/android.png")}
-            alt="Download it on the Google Play"
+            alt={context.t("Download it on the Google Play")}
           />
           <img
             src={require("images/microsoft.png")}
-            alt="Download it on the Microsoft Store"
+            alt={context.t("Download it on the Microsoft Store")}
           />
         </div>
       </div>
     </div>
   </main>
 );
+
+Auth.contextTypes = {
+  t: PropTypes.func.isRequired
+};
 
 export default Auth;
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,8 +1,49 @@
 export const translations = {
   es: {
-    Login: "Iniciar Sesion"
+    "Log in": "Iniciar Sesion",
+    "About Us": "??"
   },
   kr: {
-    Login: "입장각"
+    // Footer
+
+    "Log in": "로그인",
+    "About Us": "INSTAGRAM 정보",
+    Support: "지원",
+    Blog: "블로그",
+    Press: "홍보 센터",
+    API: "API",
+    Jobs: "채용 정보",
+    Privacy: "개인정보처리방침",
+    Terms: "약관",
+    Directory: "디렉터리",
+    Language: "언어",
+
+    // Auth
+
+    "Checkout our app. Is cool": "지금 바로 쩌는 경험을 해보세요",
+    Logo: "인스타그램 로고",
+    "Have an account?": "계정이 있으신가요?",
+    "Don't have an account?": "계정이 없으신가요?",
+    "Sign up": "가입하기",
+    "Get the app": "앱을 다운로드하세요.",
+    "Download it on the Apple Appstore": "애플 앱스토어에서 다운로드",
+    "Download it on the Google Play": "구글 플레이에서 다운로드",
+    "Download it on the Microsoft Store": "마이크로소프트 스토어에서 다운로드",
+
+    // AuthForms
+
+    Username: "사용자 이름",
+    Password: "비밀번호",
+    or: "또는",
+    "Log in with Facebook": "Facebook으로 로그인",
+    "Forgot password?": "비밀번호를 잊으셨나요?",
+
+    "Sign up to see photos and videos from your friends.":
+      "가입하고, 친구들과의 소중한 추억을 공유하세요.",
+    Email: "휴대폰 번호 또는 이메일",
+    "Full Name": "성명",
+    "By signing up, you agree to our ": "가입하면 Instagram의 ",
+    "Terms & Privacy Policy": "약관 및 개인정보처리방침",
+    ".": "에 동의하게 됩니다."
   }
 };
